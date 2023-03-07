1ce60b1097a262cad09e9f471a91506cbcc0d38c
daniil <daniilkimstachp@gmail.com>
2023-03-07 14:12:24 +0300

    Some Logic was added

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,13 +1,29 @@
-import React from "react";
-import "./search-panel.css"
+import React, { Component } from 'react';
 
-const SearchPanel = () => {
-    return (
-        <input type="text"
-               placeholder="Search"
-               className="search-input"
-        />
-    );
-};
+import './search-panel.css';
 
-export default SearchPanel;
+export default class SearchPanel extends Component {
+
+    state = {
+        term: ''
+    };
+
+    onTermChange = (e) => {
+        const {onSearchChange = () => {}} = this.props;
+        this.setState({
+            term: e.target.value
+        });
+
+        onSearchChange(e.target.value);
+    };
+
+    render() {
+        return (
+            <input type="text"
+                   className="form-control search-input"
+                   placeholder="type to search"
+                   value={this.state.term}
+                   onChange={ this.onTermChange } />
+        );
+    };
+}
